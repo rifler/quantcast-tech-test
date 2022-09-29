b31352c2ca8a9bd8f96969d10893e3a2a20db899
Dmitrii Kanatnikov <steve111@yandex.ru>
2022-09-30 00:38:02 +0300

    better xo-config

---
+++ .xo-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    space: true,
+    rules: {
+        'import/extensions': 'off',
+        // todo temp
+        'unicorn/no-empty-file': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+    },
+}
