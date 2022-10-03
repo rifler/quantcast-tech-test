3218316d546e7cadbb08a500f96fb2b452e2f42d
Dmitrii Kanatnikov <steve111@yandex.ru>
2022-10-03 23:49:45 +0300

    better memo types

--- source/utils/function.ts
+++ source/utils/function.ts
@@ -1,10 +1,11 @@
 export const noop = () => undefined;
 
-type AnyFunc = (...args: any[]) => any;
-
 const firstArg = <T>(a: T): T => a;
 
-export const memoize = <Fn extends AnyFunc>(fn: Fn, getKey?: (...arg: Parameters<Fn>) => string): Fn => {
+type AnyFunc = (...args: any[]) => any;
+type MemoFn<Fn extends AnyFunc> = (...args: Parameters<Fn>) => ReturnType<Fn>;
+
+export const memoize = <Fn extends MemoFn<Fn>>(fn: Fn, getKey?: (...arg: Parameters<Fn>) => string): Fn => {
   const memo: Record<string, ReturnType<Fn>> = {};
   type GetKey = Exclude<typeof getKey, undefined>;
   const keyFn: GetKey = getKey ?? firstArg as unknown as GetKey;
@@ -12,11 +13,9 @@ export const memoize = <Fn extends AnyFunc>(fn: Fn, getKey?: (...arg: Parameters
   return function (...args: Parameters<Fn>) {
     const key = keyFn(...args);
     if (!Object.prototype.hasOwnProperty.call(memo, key)) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       memo[key] = fn(...args);
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return memo[key];
   } as Fn;
 };
